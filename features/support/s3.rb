1af5e982b57ee1842836043a92376267e7a8fbc2
Tim Olshansky <tim.olshansky@gmail.com>
2011-09-02 13:54:30 -0700

    Ensure appropriate error is raised when testing features

--- features/support/s3.rb
+++ features/support/s3.rb
@@ -1,21 +1,21 @@
 module AWSS3Methods
   def load_s3
     begin
-      require 'aws/s3'
+      require 'aws-sdk'
     rescue LoadError => e
-      fail "You do not have aws-s3 installed."
+      fail "You do not have aws-sdk installed."
     end
   end
 
   def assert_credentials(key, secret)
     load_s3
     begin
-      AWS::S3::Base.establish_connection!(
+      aws_s3 = AWS::S3.new(
         :access_key_id => key,
         :secret_access_key => secret
       )
-      AWS::S3::Service.buckets
-    rescue AWS::S3::ResponseError => e
+      aws_s3.buckets.first
+    rescue AWS::S3::Errors::InvalidAccessKeyId => e
       fail "Could not connect using AWS credentials in AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY. " +
            "Please make sure these are set in your environment."
     end
